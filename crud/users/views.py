e3aaa332223cdfcb2ba10774642a397f823fd6f6
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-05 17:27:36 -0300

    hice cambios para el frontend, rediseno un par de cosas.

--- crud/users/views.py
+++ crud/users/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(req):
-    return HttpResponse('Index')
+    return render(req,'users/index.html',{})
 
 def create(req):
     return HttpResponse('Create User')
 
 def view(req, id):
-    return HttpResponse('View User')
+    return HttpResponse(f'View User, id: {id}')
 
 def edit(req, id):
-    return HttpResponse('Edit User')
+    return HttpResponse(f'Edit User, id: {id}')
 
 def delete(req, id):
-    return HttpResponse('Delete User')
+    return HttpResponse(f'Delete User, id: {id}')
