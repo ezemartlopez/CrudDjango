daa6d5edb4e91dc840289c481681cb08ef591c7a
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-16 15:12:27 -0300

    modifique los placeholder del UserForm, cuention de estetica de formulario.

--- crud/users/forms.py
+++ crud/users/forms.py
@@ -6,12 +6,12 @@ class UserForm(ModelForm):
         model = User
         fields = '__all__'
         widgets = {
-            'name':TextInput(attrs={'type':'text','placeholder':'Nombres', 'class':'form-control'}),
-            'last_name':TextInput(attrs={'type':'text','placeholder':'Apellidos', 'class':'form-control'}),
-            'email':EmailInput(attrs={'type':'text','placeholder':'Email', 'class':'form-control'}),
+            'name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Nombres', 'class':'form-control'}),
+            'last_name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Apellidos', 'class':'form-control'}),
+            'email':EmailInput(attrs={'type':'text','placeholder':'Ingrese el Email', 'class':'form-control'}),
             'active':CheckboxInput(attrs={'type':'checkbox', 'class':'form-check-input'}),
             'type_user':Select(attrs={'type':'select', 'class':'form-select'},choices=USER_TYPE),
-            'photo':FileInput(attrs={'type':'file','placeholder':'Foto o Imagen','class':'form-control'}),
+            'photo':FileInput(attrs={'type':'file','class':'form-control'}),
             'date_created':DateInput(attrs={'type':'date'})
 
 
