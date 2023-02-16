35d86201bd391e508f9cd1a0bb08e074b8803a3c
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-16 20:27:58 -0300

    de la funcion create lo modifique para que ahora si cargue la imagen que se sube.
    cree la funcion edit, ahora modifica el usuario y si se le pasa una imagen borra la anterior
    
    ahora la funcion delete borra el usuario y su imagen asociada.
    
    por ahora las imagenes estan en la carpeta media.

--- crud/users/views.py
+++ crud/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User
 from .forms import UserForm
+from django.contrib import messages
 # Create your views here.
 def index(req):
     users = User.objects.all()
@@ -15,36 +16,57 @@ def create(req):
         print(req.POST)
         form = UserForm(req.POST, req.FILES)
         if form.is_valid():
-            user = User.objects.create(
-                name = form.cleaned_data.get('name'),
-                last_name = form.cleaned_data.get('last_name'),
-                email = form.cleaned_data.get('email'),
-                active = form.cleaned_data.get('active'),
-                type_user = form.cleaned_data.get('type_user'),
-                photo = form.cleaned_data.get('photo')
-            )
             #si es valido guardo el nuevo usuario
-            user.save()
+            form.save()
         #por ahora redirecciono a la pagina principal
         return redirect('index')
 
     # Si solicito la pagina de create_user
     if req.method =='GET':
         form = UserForm()
-        context = {'form':form}
+        url_img = '/media/images/default/no-img.png'
+        context = {'form':form,'url_img':url_img}
         return render(req,'users/create_user.html',context)
 
 
 def view(req, id):
-    #return HttpResponse(f'View User, id: {id}')
+    #Obtengo el usuario con ese id
     user = User.objects.get(id=id)
+    #Lo agrego al contexto para que pueda ser visualizado
     context = {'user':user}
+    #Envio el template que se encarga de la vista del usuario con el contexto
     return render(req, 'users/view_user.html', context)
 
 
 def edit(req, id):
-    return HttpResponse(f'Edit User, id: {id}')
+    #Obtengo el usuario con ese id
+    user = User.objects.get(id=id)
+    url_photo = user.photo.url
+    if(req.method == 'GET'):
+        form = UserForm(instance=user)
+        context = {'form':form,'url_img':url_photo, 'id':id}
+        return render(req, 'users/edit_user.html',context)
+    if(req.method == 'POST'):
+        form = UserForm(req.POST, req.FILES, instance=user)
+        print(req.FILES)
+        if(req.FILES):
+            #esto podria eliminar la imagen por defecto controlo que no sea esa
+            if(not(user.photo.url == 'media/images/default/no-img.png')):
+                user.photo.delete()
+        #Validamos que los cambios sean validos
+        if form.is_valid():
+            form.save()
+        url_photo = User.objects.get(id=id).photo.url
+        context = {'form':form,'url_img':url_photo,'id':id}
+        messages.success(req,message='Contacto actualizado.', extra_tags='alert-success')
+        return render(req, 'users/edit_user.html',context)
 
 
 def delete(req, id):
-    return HttpResponse(f'Delete User, id: {id}')
+    #Obtengo el usuario con ese id
+    user = User.objects.get(id=id)
+    #Elimino ese usuario, pero antes elimino la foto asociada en la carpeta media/images
+    user.photo.delete()
+    user.delete()
+    #redirijo ala pagina principal
+    return redirect('index')
