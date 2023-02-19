b1dec57f3138a56cbf548a97d11720baa9e4633c
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-19 16:58:42 -0300

    me olvide agregar los nuevos campos del modelo.

--- crud/users/forms.py
+++ crud/users/forms.py
@@ -9,10 +9,10 @@ class UserForm(ModelForm):
             'name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Nombres', 'class':'form-control'}),
             'last_name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Apellidos', 'class':'form-control'}),
             'email':EmailInput(attrs={'type':'text','placeholder':'Ingrese el Email', 'class':'form-control'}),
-            'active':CheckboxInput(attrs={'type':'checkbox', 'class':'form-check-input'}),
-            'type_user':Select(attrs={'type':'select', 'class':'form-select'},choices=USER_TYPE),
+            'active':CheckboxInput(attrs={'type':'checkbox', 'class':'checkboxes'}),
+            'type_user':Select(attrs={'type':'select', 'class':'form-control'},choices=USER_TYPE),
             'photo':FileInput(attrs={'type':'file','id':'seleccionArchivos','class':'form-control'}),
-            'date_created':DateInput(attrs={'type':'date'})
-
-
+            'date_created':DateInput(attrs={'type':'date'}),
+            'address':TextInput(attrs={'type':'text','placeholder':'Ingrese su direccion', 'class':'form-control'}),
+            'phone':TextInput(attrs={'type':'text','placeholder':'Ingrese su telefono', 'class':'form-control'})
         }
