b3d8ec6a8da51c698e0a5bfaf770f1c9ba3e0d6f
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-21 16:01:49 -0300

    agregue la clase checkbox al campo active.

--- crud/users/forms.py
+++ crud/users/forms.py
@@ -9,7 +9,7 @@ class UserForm(ModelForm):
             'name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Nombres', 'class':'form-control'}),
             'last_name':TextInput(attrs={'type':'text','placeholder':'Ingrese los Apellidos', 'class':'form-control'}),
             'email':EmailInput(attrs={'type':'text','placeholder':'Ingrese el Email', 'class':'form-control'}),
-            'active':CheckboxInput(attrs={'type':'checkbox'}),
+            'active':CheckboxInput(attrs={'type':'checkbox','class':'checkbox'}),
             'type_user':Select(attrs={'type':'select', 'class':'form-control'},choices=USER_TYPE),
             'photo':FileInput(attrs={'type':'file','id':'seleccionArchivos','class':'form-control'}),
             'date_created':DateInput(attrs={'type':'date'}),
