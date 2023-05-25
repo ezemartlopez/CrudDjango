f7136453d34a8e0d48581d865147afd0c5316f0f
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-05-24 22:01:31 -0300

    cambios

--- crud/static/scripts/load_img.js
+++ crud/static/scripts/load_img.js
@@ -1,12 +1,12 @@
 // Obtener referencia al input y a la imagen
-
-const $seleccionArchivos = document.querySelector("#seleccionArchivos"),
-  $imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
+const $seleccionArchivos = document.querySelector("#seleccionArchivos");
+const $imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
 
 // Escuchar cuando cambie
-$seleccionArchivos.addEventListener("change", () => {
+  $seleccionArchivos.addEventListener("change", () => {
   // Los archivos seleccionados, pueden ser muchos o uno
   const archivos = $seleccionArchivos.files;
+
   // Si no hay archivos salimos de la función y quitamos la imagen
   if (!archivos || !archivos.length) {
     $imagenPrevisualizacion.src = "";
@@ -14,8 +14,10 @@ $seleccionArchivos.addEventListener("change", () => {
   }
   // Ahora tomamos el primer archivo, el cual vamos a previsualizar
   const primerArchivo = archivos[0];
+
   // Lo convertimos a un objeto de tipo objectURL
   const objectURL = URL.createObjectURL(primerArchivo);
+
   // Y a la fuente de la imagen le ponemos el objectURL
   $imagenPrevisualizacion.src = objectURL;
 });
--- crud/static/scripts/table.js
+++ crud/static/scripts/table.js
@@ -6,19 +6,27 @@ $(document).ready(function(){
 	
 	
 
-	// Select/Deselect checkboxes
+	// Select/Deselect checkboxes All
 	var checkbox = $('table tbody input[type="checkbox"]');
 	$("#selectAll").click(function(){
+		//Si se selecciona accede a todos los checkbox y los pone en True
 		if(this.checked){
 			checkbox.each(function(){
-				this.checked = true;                        
+				this.checked = true;
+				alert("Se seleccionaron todos los elementos");
+				//Aca puede acceder a otro valor del checkbox y agregarlo a una lista
+				//........                       
 			});
-		} else{
+		} 
+		else{
+			//Si esta en falso accede a todos los checkbox y los pone en False
 			checkbox.each(function(){
 				this.checked = false;                        
 			});
 		} 
 	});
+
+	//Select/Deselect checkbox Individual
 	checkbox.click(function(){
 		if(!this.checked){
 			$("#selectAll").prop("checked", false);
